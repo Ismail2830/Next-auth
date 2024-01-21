ef6d524bc55c46e1692780a13006b4971882a712
Ismail2830 <ismailaitrehail2830@gmail.com>
2024-01-21 23:48:17 +0100

    callback session

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { Session } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { getUserById } from "./data/user"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
 
@@ -15,10 +16,20 @@ export const {
       if(token.sub && session.user){
         session.user.id = token.sub;
       }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
      
       return session
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role
       return token
     }
   },
